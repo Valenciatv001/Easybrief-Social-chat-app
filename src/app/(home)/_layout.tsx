f452e77173da2a6319108039b78aaedd4c3a879e
ezekiel <irechukwuezekiel@gmail.com>
2024-07-16 15:17:42 +0100

    setup stream ui chat

---
+++ src/app/(home)/_layout.tsx
@@ -0,0 +1,37 @@
+import { Slot, Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { StreamChat } from "stream-chat";
+import { OverlayProvider, Chat } from "stream-chat-expo";
+
+const client = StreamChat.getInstance("z6m66pajgwye");
+
+export default function HomeLayout() {
+  useEffect(() => {
+    const connect = async () => {
+      await client.connectUser(
+        {
+          id: "jlahey",
+          name: "Jim Lahey",
+          image: "https://i.imgur.com/fR9Jz14.png",
+        },
+        client.devToken("jlahey")
+      );
+
+      // const channel = client.channel("messaging", "the_park", {
+      //   name: "The Park",
+      // });
+      // await channel.watch();
+    };
+
+    connect();
+  });
+  return (
+    <OverlayProvider>
+      <Chat client={client}>
+        <Stack>
+          <Stack.Screen name="(tabsScreen)" options={{ headerShown: false }} />
+        </Stack>
+      </Chat>
+    </OverlayProvider>
+  );
+}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,34 +1,16 @@
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import "react-native-reanimated";
+import { Slot, Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <GestureHandlerRootView>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slot />
     </GestureHandlerRootView>
   );
 }
+
+// import { Slot, Stack } from "expo-router";
+
+// export default function RootLayout() {
+//   return <Slot />;
+// }
